979c0839007a31f12cc3f232013384a7b56e677a
ikasumi_wt <wtsnyk13@gmail.com>
2016-11-09 01:49:56 +0900

    add encoding.js

---
+++ nodejs_lib/buffer/src/encoding.js
@@ -0,0 +1,10 @@
+const buf = Buffer.from('hello world', 'ascii');
+
+// Prints: 68656c6c6f20776f726c64
+console.log(buf.toString('hex'));
+
+// Prints: aGVsbG8gd29ybGQ=
+console.log(buf.toString('base64'));
+
+
+console.log(buf.toString('utf8'));
