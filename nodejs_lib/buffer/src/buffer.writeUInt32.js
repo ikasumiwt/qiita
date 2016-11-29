fe4d2e163004581d30817257b5544c5e9be2b7e4
Yusuke Kato <wtsnyk13@gmail.com>
2016-11-30 00:36:45 +0900

    update

--- nodejs_lib/buffer/src/buffer.writeUInt32.js
+++ nodejs_lib/buffer/src/buffer.writeUInt32.js
@@ -7,18 +7,16 @@ var intArr = [
                 ];
 var buf = Buffer.allocUnsafe(4);;
 
-buf.writeUInt16LE(0x00FF, 0);
-buf.writeUInt16LE(0x00FF, 2);
+buf.writeUInt32BE(0x0011aaff, 0);
+
 console.log(buf);
+
 
 //2byte以上を書き込もうとするとエラー
 //buf.writeUInt8(0xffff, 2);
 
-buf.writeUInt16BE(0x00FF, 0);
-buf.writeUInt16BE(0x00FF, 2);
-/*
-  0,255,255
-*/
+buf.writeUInt32LE(0x0011aaff, 0);
+
 console.log(buf);
 
 
